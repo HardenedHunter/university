b29ba87a5a69c1c7ab547e6e4b6b44230d36134d
Artyom Khudyakov <47335768+HardenedHunter@users.noreply.github.com>
2020-03-26 03:31:16 +0300

    CarInfo and CarNumber added

---
+++ ADS/Hashing/Hashing/CarInfo.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace Hashing
+{
+    class FileUtils
+    {
+        public static bool GetValueFromFile(StreamReader reader, ref string value)
+        {
+            value = "";
+            bool result = !reader.EndOfStream;
+            if (result)
+            {
+                value = reader.ReadLine();
+                if (value == null) throw new NullReferenceException("Ошибка чтения из файла.");
+                int colonIndex = value.IndexOf(':');
+                result = colonIndex >= 0 && colonIndex + 1 < value.Length;
+                if (result)
+                {
+                    value = value.Substring(colonIndex + 1).Trim();
+                }
+            }
+
+            return result;
+        }
+    }
+
+    class CarInfo
+    {
+        private string _model;
+        private string _owner;
+        private CarNumber _number;
+
+        public string Model
+        {
+            get => _model;
+            private set
+            {
+                if (value.Length == 0)
+                    throw new ArgumentException("Модель автомобиля не может быть пустой.");
+                _model = value;
+            }
+        }
+        public string Owner
+        {
+            get => _owner;
+            private set
+            {
+                if (value.Length == 0)
+                    throw new ArgumentException("Владелец автомобиля не может быть пустым.");
+                _owner = value;
+            }
+        }
+
+        public CarInfo(string model, string owner, CarNumber number)
+        {
+            Model = model;
+            Owner = owner;
+            _number = number;
+        }
+
+        public static bool TryReadAsText(StreamReader reader, ref CarInfo carInfo)
+        {
+            string tmpModel = "", tmpOwner = "";
+            var tmpNumber = new CarNumber();
+                           
+            bool result = CarNumber.TryReadAsText(reader, ref tmpNumber) && 
+                          FileUtils.GetValueFromFile(reader, ref tmpModel) &&
+                          FileUtils.GetValueFromFile(reader, ref tmpOwner);
+            if (result)
+                carInfo = new CarInfo(tmpModel, tmpOwner, tmpNumber);
+            return result;
+        }
+    }
+
+    class CarNumber
+    {
+        //Код региона
+        public int RegionCode { get; set; }
+        //Регистрационный номер
+        public int NumberCode { get; set; }
+        //Серия
+        private string _series;
+        public string Series
+        {
+            get => _series;
+            set
+            {
+                if (value.Length != 3)
+                    throw new ArgumentException("Длина серии должна быть равна 3.");
+                _series = value;
+            }
+        }
+
+        //Разрешённые в серии буквы 
+        private static readonly char[] ValidSeriesLetters = {'А', 'Е', 'У', 'В', 'О', 'Р', 'К', 'Н', 'Х', 'Т', 'С', 'М'};
+
+        public CarNumber(int regionCode = 36, int numberCode = 001, string series = "ЕКХ")
+        {
+            RegionCode = regionCode;
+            NumberCode = numberCode;
+            Series = series;
+        }
+
+        public override string ToString()
+        {
+            return Series[0] + " " + NumToStr(NumberCode, 3) + " " + Series[1] + Series[2] + " " +
+                   NumToStr(RegionCode, 2);
+        }
+
+        /// <summary>
+        /// Конвертация числа в строку с заполнением недостающих разрядов нулями.
+        /// </summary>
+        /// <param name="num">Число.</param>
+        /// <param name="minLength">Минимально заданное кол-во разрядов.</param>
+        /// <returns>Строковое представление числа.</returns>
+        private static string NumToStr(int num, int minLength)
+        {
+            string result = num.ToString();
+            while (result.Length < minLength)
+                result = '0' + result;
+            return result;
+        }
+
+        public static bool TryReadAsText(StreamReader reader, ref CarNumber number)
+        {
+            string tmpNumber = "";
+            return FileUtils.GetValueFromFile(reader, ref tmpNumber) && TryStrToCarNumber(tmpNumber, ref number);
+        }
+
+        public static bool TryStrToCarNumber(string source, ref CarNumber number)
+        {
+            source = source.ToUpper();
+            string tmpSeries = "";
+            int tmpRegionCode = 0, tmpNumberCode = 0;
+            bool result = GetSeriesChar(ref source, ref tmpSeries) &&
+                          GetNumber(ref source, 1, 999, ref tmpNumberCode) &&
+                          GetSeriesChar(ref source, ref tmpSeries) && GetSeriesChar(ref source, ref tmpSeries) &&
+                          GetNumber(ref source, 1, 999, ref tmpRegionCode) &&
+                          source.Trim() == "";
+            if (result)
+                number = new CarNumber(tmpRegionCode, tmpNumberCode, tmpSeries);
+            return result;
+        }
+
+        private static bool GetSeriesChar(ref string source, ref string series)
+        {
+            source = source.Trim();
+            bool result = source != "" && ValidSeriesLetters.Contains(source[0]);
+            if (result)
+            {
+                series += source[0];
+                source = source.Remove(0, 1);
+            }
+            return result;
+        }
+
+        private static bool GetNumber(ref string source, int min, int max, ref int number)
+        {
+            source = source.Trim();
+            int length = source.Length;
+            number = 0;
+            bool result = length > 0 && source[0] >= '0' && source[0] <= '9';
+            if (result)
+            {
+                int i = 0;
+                do
+                {
+                    number += (int)char.GetNumericValue(source[i]);
+                    i++;
+                } while (i < length && source[0] >= '0' && source[0] <= '9');
+                source = source.Remove(0, i);
+                result = number >= min && number <= max;
+            }
+            return result;
+        }
+    }
+}
